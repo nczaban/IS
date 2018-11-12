278bc88947aa767d51db78b5c4efd3598f12bf3a
nczaban19 <nczaban19@wooster.edu>
2018-11-12 10:34:39 -0500

    Comment out extraneous functions

--- decisionTrees.m
+++ decisionTrees.m
@@ -30,27 +30,25 @@ end
 
 testData = M(:,3:end);
 T = fitctree(testData(1:4000,:), p1Wins(1:4000));
-
-PrunedT = prune(T, 'level', 35);
-view(PrunedT, 'Mode', 'Graph')
-P=predict(PrunedT, testData(4001:4500, :));
-mse=sum((p1Wins(4001:4500)-P).^2)/length(P)
-
-pause
-
-j=1;
-for i=1:gameNums
-    if(testData(i, 96) > 20.5)
-        trainingTurnNums(j,1) = testData(i,1);
-        trainingTurnNumsSolutions(j,1) = p1Wins(i,1);
-        trainingTurnNums(j,2) = 96 - p2DataStart(i,1);
-        j=j+1;
-    end
-end
-
-for
-
-TwoColumnT = fitctree(trainingTurnNums(1:400,:), trainingTurnNumsSolutions(1:400));
-view(TwoColumnT, 'Mode', 'Graph')
-P=predict(TwoColumnT, trainingTurnNums(401:488, :));
-mse=sum((trainingTurnNumsSolutions(401:488,1)-P).^2)/length(P)
+view(T, 'Mode', 'Graph')
+% PrunedT = prune(T, 'level');
+% view(PrunedT, 'Mode', 'Graph')
+% P=predict(PrunedT, testData(4001:4500, :));
+% mse=sum((p1Wins(4001:4500)-P).^2)/length(P)
+% 
+% pause
+% 
+% j=1;
+% for i=1:gameNums
+%     if(testData(i, 96) > 20.5)
+%         trainingTurnNums(j,1) = testData(i,1);
+%         trainingTurnNumsSolutions(j,1) = p1Wins(i,1);
+%         trainingTurnNums(j,2) = 96 - p2DataStart(i,1);
+%         j=j+1;
+%     end
+% end
+% 
+% TwoColumnT = fitctree(trainingTurnNums(1:400,:), trainingTurnNumsSolutions(1:400));
+% view(TwoColumnT, 'Mode', 'Graph')
+% P=predict(TwoColumnT, trainingTurnNums(401:488, :));
+% mse=sum((trainingTurnNumsSolutions(401:488,1)-P).^2)/length(P)
