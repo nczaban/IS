6b58e67c1e1e442305262f5430d88a35223448a7
nczaban19 <nczaban19@wooster.edu>
2018-11-14 11:50:24 -0500

    Change data generator to allow ranges of agressiveness

--- dataGenerator2.py
+++ dataGenerator2.py
@@ -1,42 +1,47 @@
 # Nicholas Czaban
 # The following functions run a game with randomly-generated actions. The two possible actions
 # are healing and attacking. Both players begin with 25 hit points. An attack removes 5 hit points,
-# and healing restores 4 hit points.
+# and healing restores 4 hit points. The first player has a 50-50 chance of either action. The second
+# player has a randomly generated "agressiveness" value, determining how likely they are to heal.
 from random import random
 
 def gameLoop():
     hitPoints = [25, 25]
     turnCounter = 0
     isPlayer0Turn = True
+    agressiveness = random()
     healthLevels = []
     healthLevels.append(hitPoints.copy())
+    
     newAction = ''
     while (hitPoints[0]>0) and (hitPoints[1]>0):
         turnCounter += 1
         r = random()
-        if(r<0.33):
-            newAction = 'A'
-        elif(r<0.67):
-            newAction = 'H'
-        else:
-            newAction = 'N'
         if(isPlayer0Turn):
-            if(newAction == 'H'):
+            if(r<0.5):
+                newAction = 'A'
+            else:
+                newAction = 'H'
+            if(newAction == 'H' and hitPoints[0] <= 25):
                 hitPoints[0] += 4
                 if(hitPoints[0] > 25):
                     hitPoints[0] = 25
-            elif(newAction == 'A'):
-                hitPoints[1] -= 5
+                else:
+                    hitPoints[1] -= 5
         else:
-            if(newAction == 'H'):
+            if(r<agressiveness):
+                newAction = 'A'
+            else:
+                newAction = 'H'
+            if(newAction == 'H' and hitPoints[1] <= 25):
                 hitPoints[1] += 4
                 if(hitPoints[1] > 25):
                     hitPoints[1] = 25
-            elif(newAction == 'A'):
-                hitPoints[0] -= 5
+                else:
+                    hitPoints[0] -= 5
         isPlayer0Turn = not isPlayer0Turn
         healthLevels.append(hitPoints.copy())
-    output = [hitPoints[0], hitPoints[1], turnCounter]
+    output = [hitPoints[0], hitPoints[1], turnCounter, agressiveness]
     for i in healthLevels:
         output.append(i)
     # output.append('x')
