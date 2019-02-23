7a6331b69b7b4f0dc1e19bdbdafdfeef295fb0ee
nczaban19 <nczaban19@wooster.edu>
2019-02-22 20:15:05 -0500

    Change sprites on parry, tweak AI models

--- game3.py
+++ game3.py
@@ -61,6 +61,7 @@ def main():
     screen = pygame.display.set_mode(screen_res)
     background_path = os.path.join('sprites', 'CaveTest.png')
     character_path = os.path.join('sprites', 'PlayerTest.png')
+    player_parry_path = os.path.join('sprites', 'PlayerTest_Parry.png')
     loss_path = os.path.join('sprites', 'GameOver.png')
     win_path = os.path.join('sprites', 'Victory.png')
     menu_path = os.path.join('sprites', 'Blue.png')
@@ -73,6 +74,8 @@ def main():
     win = pygame.transform.scale(win, screen.get_size())
     character = pygame.image.load(character_path).convert_alpha()
     character = pygame.transform.scale(character, (128, 256))
+    player_parry = pygame.image.load(player_parry_path).convert_alpha()
+    player_parry = pygame.transform.scale(player_parry, (128, 256))
     
     player = PlayerAvatar(1)
     enemy = PlayerAvatar(3)
@@ -90,34 +93,44 @@ def main():
             event = pygame.event.wait()
             ai_model = menu.check_input(event)
             if ai_model == menu_items[0]:
-                enemy = PlayerAvatar(0)
+                enemy = PlayerAvatar(1)
                 enemy_path = os.path.join('sprites', 'Goblin.png')
                 enemy_sprite = pygame.image.load(enemy_path).convert_alpha()
                 enemy_sprite = pygame.transform.scale(enemy_sprite, (128, 256))
+                guard_path = os.path.join('sprites', 'Goblin_Parry.png')
+                guard_sprite = pygame.image.load(guard_path).convert_alpha()
+                guard_sprite = pygame.transform.scale(guard_sprite, (128, 256))
                 display_menu = False
             elif ai_model == menu_items[1]:
-                enemy = PlayerAvatar(1)
+                enemy = PlayerAvatar(2)
                 enemy_path = os.path.join('sprites', 'Troll.png')
                 enemy_sprite = pygame.image.load(enemy_path).convert_alpha()
                 enemy_sprite = pygame.transform.scale(enemy_sprite, (128, 256))
                 display_menu = False
             elif ai_model == menu_items[2]:
-                enemy = PlayerAvatar(2)
+                enemy = PlayerAvatar(3)
                 enemy_path = os.path.join('sprites', 'Orc.png')
                 enemy_sprite = pygame.image.load(enemy_path).convert_alpha()
                 enemy_sprite = pygame.transform.scale(enemy_sprite, (128, 256))
                 display_menu = False
             elif ai_model == menu_items[3]:
-                enemy = PlayerAvatar(3)
+                enemy = PlayerAvatar(4)
                 enemy_path = os.path.join('sprites', 'Elf.png')
                 enemy_sprite = pygame.image.load(enemy_path).convert_alpha()
                 enemy_sprite = pygame.transform.scale(enemy_sprite, (128, 256))
+                guard_path = os.path.join('sprites', 'Elf_Parry.png')
+                guard_sprite = pygame.image.load(guard_path).convert_alpha()
+                guard_sprite = pygame.transform.scale(guard_sprite, (128, 256))
+
                 display_menu = False
             elif ai_model == menu_items[4]:
-                enemy = PlayerAvatar(4)
+                enemy = PlayerAvatar(5)
                 enemy_path = os.path.join('sprites', 'Magician.png')
                 enemy_sprite = pygame.image.load(enemy_path).convert_alpha()
                 enemy_sprite = pygame.transform.scale(enemy_sprite, (128, 256))
+                guard_path = os.path.join('sprites', 'Magician_Parry.png')
+                guard_sprite = pygame.image.load(guard_path).convert_alpha()
+                guard_sprite = pygame.transform.scale(guard_sprite, (128, 256))
                 display_menu = False
 
         game_over = False
@@ -131,12 +144,20 @@ def main():
 
         while not game_over:
             screen.blit(background, min_coord)
-            screen.blit(character, (100, 400))
-            screen.blit(enemy_sprite, (500, 400))
+            if player.isParrying:
+                screen.blit(player_parry, (100, 400))
+            else:
+                screen.blit(character, (100, 400))
+
+            if enemy.isParrying:
+                screen.blit(guard_sprite, (500, 400))
+            else:
+                screen.blit(enemy_sprite, (500, 400))
 
             stat_string1 = "Player: " + str(hitPoints[0]) + "   "
             stat_string2 = "Enemy: " + str(hitPoints[1])
-            menu_items = ['Attack', 'Strong Attack', 'Parry', 'Heal']
+            stat_string3 = "Heal (" + str(player.healTurns) + " left)"
+            menu_items = ['Attack', 'Strong Attack', 'Parry', stat_string3]
             ability = Menu(stat_string1 + stat_string2, menu_path, (500, 350), font_file, (250, 25))
             for item in menu_items:
                 ability.add_item(item)
@@ -150,8 +171,14 @@ def main():
                     event = pygame.event.wait()
                     user_input = ability.check_input(event)
                     screen.blit(background, min_coord)
-                    screen.blit(character, (100, 400))
-                    screen.blit(enemy_sprite, (500, 400))
+                    if player.isParrying:
+                        screen.blit(player_parry, (100, 400))
+                    else:
+                        screen.blit(character, (100, 400))
+                    if enemy.isParrying:
+                        screen.blit(guard_sprite, (500, 400))
+                    else:
+                        screen.blit(enemy_sprite, (500, 400))
 
                     if(user_input == menu_items[3] and hitPoints[0] <= 25 and player.healTurns>0):
                         hitPoints[0] += 4
@@ -203,8 +230,17 @@ def main():
                     elif(user_input == menu_items[2]):
                         player.isParrying = True
                         print("Player is preparing to parry")
+                        screen.blit(background, min_coord)
+                        if player.isParrying:
+                            screen.blit(player_parry, (100, 400))
+                        else:
+                            screen.blit(character, (100, 400))
+
+                        if enemy.isParrying:
+                            screen.blit(guard_sprite, (500, 400))
+                        else:
+                            screen.blit(enemy_sprite, (500, 400))
                         display_menu = False
-                        
             else:
                 sleep(.5)
                 enemy.isParrying = False
@@ -279,6 +315,8 @@ def main():
             sleep(3)
         output = [ai_model, hitPoints, turnCounter]
         writeGame(output, healthLevels)
+
+
         
 class PlayerAvatar:
     def __init__(self, model):
@@ -293,15 +331,17 @@ class PlayerAvatar:
     # information
     def helperAIFunc(self, enemyHP):
         if self.AI_MODEL == 1:
-            return self.limitedRandomAI()
+            return self.randomAI()
         elif self.AI_MODEL == 2:
             return self.aggressiveRandomAI()
         elif self.AI_MODEL == 3:
             return self.fiftyPercentAI()
         elif self.AI_MODEL == 4:
-            return self.comparitiveAI(enemyHP)
+            return self.comparativeAI(enemyHP)
+        elif self.AI_MODEL == 5:
+            return self.scalingDifficulty(enemyHP)
         else:
-            return self.randomAI()
+            return self.limitedRandomAI()
             
     # AI Model 1 - Ramdonly choose to attack or heal. If no heal turns remain, the AI does a
     # normal attack
@@ -330,17 +370,21 @@ class PlayerAvatar:
         return newAction
 
     # AI Model 3 - Use agressiveness statistic to determine how frequently the AI attacks.
-    # Only the basic attack is available in this model.
+    # If the AI is randomly chosen to heal, but has expended all healing turns, it uses a
+    # strong attack instead.
     def aggressiveRandomAI(self):
         newAction = ""
         r=random()
         if r<self.aggressiveness and self.healTurns > 0:
             newAction = "H"
+        elif r<self.aggressiveness:
+            newAction = "S"
         else:
             newAction = "A"
         return newAction
 
-    # AI Model 4 - Whenever the HP of the AI drops below 50%, the AI heals if possible.
+    # AI Model 4 - Whenever the HP of the AI drops below 50%, the AI heals if possible. Else,
+    # the AI does a normal
     def fiftyPercentAI(self):
         newAction = ""
         if (1.0*self.hp/self.maxHP < .5) and (self.healTurns > 0):
@@ -349,13 +393,39 @@ class PlayerAvatar:
             newAction = "A"
         return newAction
 
-    # AI Model 5 - Whenever the HP of the AI drops below the HP of the opponent, 
+    # AI Model 5 - Whenever the HP of the AI drops below the HP of the opponent, the AI tries
+    # to heal. If no heal turns remain, do a strong attack
     def comparativeAI(self, enemyHP):
         newAction = ""
         if (self.hp < enemyHP) and (self.healTurns > 0):
             newAction = "H"
+        elif (self.hp < enemyHP):
+            newAction = "P"
         else:
             newAction = "A"
+        return newAction
+
+    # AI Model 6 - When the AI's HP first drops below 50% of maxHP, Heal. After that, 
+    # when HP drops below 50% and the AI HP is lower than the player's HP, it heals a
+    # second time. If the AI is below 50% but above the player, they use Strong Attacks.
+    # If the AI is below the player but has no heal turns, they Parry.
+    def scalingDifficulty(self, enemyHP):
+        newAction = ""
+        if (1.0*self.hp/self.maxHP < .5) and (self.healTurns == 2):
+            newAction = "H"
+        elif (1.0*self.hp/self.maxHP >= .5):
+            newAction = "A"
+        elif (self.hp < enemyHP) and (self.healTurns == 1):
+            newAction = "H"
+        elif (self.hp >= enemyHP) and (1.0*self.hp/self.maxHP < .5):
+            newAction = "S"
+        elif (self.hp < enemyHP) and (self.healTurns == 0):
+            newAction = "P"
+        else:
+            newAction = "A"
+        return newAction
+
+
 def writeGame(output, healthLevels):
     for i in healthLevels:
         output.append(i)
@@ -365,6 +435,7 @@ def writeGame(output, healthLevels):
         file.write(line)
         file.write('\n')
 
+    
 def userInput(hitPoints):
     print("----------------------------------")
     print("Player 1: ", hitPoints[0])
@@ -373,4 +444,6 @@ def userInput(hitPoints):
     action = action.upper()
     return action
 
+
 main()
+
