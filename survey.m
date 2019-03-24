6300c890fc495f0f585f51c44979dbac03e0e2eb
nczaban19 <nczaban19@wooster.edu>
2019-03-23 22:49:25 -0400

    Calculate average HP of player at the end of game, seperated by model type

--- survey.m
+++ survey.m
@@ -21,6 +21,8 @@ t=1;
 o=1;
 e=1;
 m=1;
+b=1;
+k=1;
 
 for i=1:size(finalHP)
     if finalHP(i,1)>finalHP(i,2)
@@ -52,8 +54,16 @@ for i=1:size(finalHP)
         magicianWins(m,1)=finalHP(i, 3);
         m=m+1;
     end
+    if txt(i, 1)=="Beserker"
+        beserkerWins(b, 1)=finalHP(i, 3);
+        b=b+1;
+    end
+    if txt(i, 1)=="Knight"
+        knightWins(k, 1)=finalHP(i, 3);
+        k=k+1;
+    end
 end
-clear g o t e m i
+clear g o t e m i b k
 
 % counts = hist(goblinWins);
 % pie(counts)
@@ -64,12 +74,18 @@ clear g o t e m i
 % counts = hist(orcWins);
 % pie(counts)
 % pause
-counts = hist(elfWins);
-pie(counts)
-pause
-counts = hist(magicianWins);
-pie(counts)
-pause
+% counts = hist(elfWins);
+% pie(counts)
+% pause
+% counts = hist(magicianWins);
+% pie(counts)
+% pause
+% counts = hist(beserkerWins);
+% pie(counts)
+% pause
+% counts = hist(knightWins);
+% pie(counts)
+% pause
 
 %% Analyze the proportion of player actions per game
 [x, y]=size(txt);
@@ -98,6 +114,8 @@ t=1;
 o=1;
 e=1;
 m=1;
+b=1;
+k=1;
 
 for i=1:size(finalHP)
     if txt(i,1)=="Goblin"
@@ -120,36 +138,115 @@ for i=1:size(finalHP)
         antiMagicianActions(m,:)=actionCounts(i,:);
         m=m+1;
     end
+    if txt(i, 1)=="Beserker"
+        antiBeserkerActions(b,:)=actionCounts(i,:);
+        b=b+1;
+    end
+    if txt(i, 1)=="Knight"
+        antiKnightActions(k,:)=actionCounts(i,:);
+        k=k+1;
+    end
 end
-clear g o t e m i j x y
+clear g o t e m i j x y b k
 
-[x,y] = size(antiGoblinActions);
-for i=1:y
-    A(i) = sum(antiGoblinActions(:,i));
-end
-%pie(A)
+% [x,y] = size(antiGoblinActions);
+% for i=1:y
+%     A(i) = sum(antiGoblinActions(:,i));
+% end
+% pie(A)
+% pause
+% 
+% [x,y] = size(antiTrollActions);
+% for i=1:y
+%     A(i) = sum(antiTrollActions(:,i));
+% end
+% pie(A)
+% 
+% [x,y] = size(antiOrcActions);
+% for i=1:y
+%     A(i) = sum(antiOrcActions(:,i));
+% end
+% pie(A)
+% 
+% [x,y] = size(antiElfActions);
+% for i=1:y
+%     A(i) = sum(antiElfActions(:,i));
+% end
+% pie(A)
+% pause
+% 
+% [x,y] = size(antiMagicianActions);
+% for i=1:y
+%     A(i) = sum(antiMagicianActions(:,i));
+% end
+% pie(A)
+% 
+% [x,y] = size(antiBeserkerActions);
+% for i=1:y
+%     A(i) = sum(antiBeserkerActions(:,i));
+% end
+% pie(A)
+% pause
+% 
+% [x,y] = size(antiKnightActions);
+% for i=1:y
+%     A(i) = sum(antiKnightActions(:,i));
+% end
+% pie(A)
 
-[x,y] = size(antiTrollActions);
-for i=1:y
-    A(i) = sum(antiTrollActions(:,i));
-end
-%pie(A)
+%% Analyze remaining HP for games where Player won
+g=1;
+t=1;
+o=1;
+e=1;
+m=1;
+b=1;
+k=1;
 
-[x,y] = size(antiOrcActions);
-for i=1:y
-    A(i) = sum(antiOrcActions(:,i));
+for i=1:size(finalHP)
+    if txt(i,1)=="Goblin"
+        goblinHP(g,1:2)=finalHP(i, 1:2);
+        g=g+1;
+    end
+    if txt(i, 1)=="Troll"
+        trollHP(t,1:2)=finalHP(i, 1:2);
+        t=t+1;
+    end
+    if txt(i, 1)=="Orc"
+        orcHP(o,1:2)=finalHP(i, 1:2);
+        o=o+1;
+    end
+    if txt(i, 1)=="Elf"
+        elfHP(e,1:2)=finalHP(i, 1:2);
+        e=e+1;
+    end
+    if txt(i, 1)=="Magician"
+        magicianHP(m, 1:2)=finalHP(i, 1:2);
+        m=m+1;
+    end
+    if txt(i, 1)=="Beserker"
+        beserkerHP(b, 1:2)=finalHP(i, 1:2);
+        b=b+1;
+    end
+    if txt(i, 1)=="Knight"
+        knightHP(k, 1:2)=finalHP(i, 1:2);
+        k=k+1;
+    end
 end
-%pie(A)
 
-[x,y] = size(antiElfActions);
-for i=1:y
-    A(i) = sum(antiElfActions(:,i));
-end
-pie(A)
-pause
+[x,y] = size(goblinHP);
+avgGoblinHP = sum(goblinHP(:, 1)) / x;
+[x,y] = size(trollHP);
+avgTrollHP = sum(trollHP(:, 1)) / x;
+[x,y] = size(orcHP);
+avgOrcHP = sum(orcHP(:, 1)) / x;
+[x,y] = size(elfHP);
+avgElfHP = sum(elfHP(:, 1)) / x;
+[x,y] = size(magicianHP);
+avgMagicianHP = sum(magicianHP(:, 1)) / x;
+[x,y] = size(beserkerHP);
+avgBeserkerHP = sum(beserkerHP(:, 1)) / x;
+[x,y] = size(knightHP);
+avgKnightHP = sum(knightHP(:, 1)) / x;
 
-[x,y] = size(antiMagicianActions);
-for i=1:y
-    A(i) = sum(antiMagicianActions(:,i));
-end
-pie(A)
+clear g o t e m i b k
